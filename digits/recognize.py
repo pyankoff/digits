e96bb558d202a561e7b5bc147c31d8f3df65e9cb
Pyankov Andrey <pyankov@st-37.idms-finance.local>
2014-05-29 14:17:00 +0400

    added photo digits recognizer

---
+++ digits/recognize.py
@@ -0,0 +1,57 @@
+import numpy as np
+from learning_class import neuralNetwork
+from scipy import misc
+import matplotlib.pyplot as plt
+
+np.seterr(over='ignore')
+
+def recognizeDigit():
+    input_layer_size = 784
+    hidden_layer_size = 25
+    out_layer_size = 10
+    lmbd = 0.1
+
+    Theta1 = np.loadtxt('T1_out.csv', delimiter=',')
+    Theta2 = np.loadtxt('T2_out.csv', delimiter=',')
+    nn_params = np.hstack((Theta1.ravel(order='F'), Theta2.ravel(order='F')))
+
+    X = np.loadtxt('X_5000.csv', delimiter=',')
+    y = np.loadtxt('y_5000.csv', delimiter=',')
+
+    a = neuralNetwork(nn_params, hidden_layer_size, out_layer_size,
+                      X, y, lmbd)
+
+    for digitcase in range(10):
+        filename = 'digit pics/%d.PNG' % digitcase
+        raw_image = misc.imread(filename, flatten=1)
+
+        plt.imshow(raw_image, cmap=plt.cm.gray)
+        plt.show()
+        
+        raw_image = np.where(raw_image < np.mean(raw_image) - 
+                             np.std(raw_image), 0, 255)
+
+        
+        t1 = np.zeros((28, 145))
+
+        # 145x145 to 28x28 transition array
+        for i in range(145):
+            for j in range(28):
+                if int(i * 28 / 145) == j:
+                    t1[j,i] = 1
+                else:
+                    t1[j,i] = 0
+
+        b = np.dot(t1, np.dot(raw_image, t1.T))
+        b = np.where(b < np.mean(b) - np.std(b), 0, 255)
+
+        image = 255 - b
+
+        res = np.argmax(a.forwardProp(Theta1, Theta2, image.reshape(
+                    (1, input_layer_size)))[0])
+
+        print "Recognized digit: ", res
+        wait = raw_input("press enter to continue")
+
+
+recognizeDigit()
